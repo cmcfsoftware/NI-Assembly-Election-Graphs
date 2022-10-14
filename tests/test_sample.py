34144341900fa7b0e21e13917b02cd6363084f92
Conor <conormcfeely@gmail.com>
2022-10-14 19:12:56 +0100

    Added basic tests to check responses of valid routes

---
+++ tests/test_sample.py
@@ -0,0 +1,24 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from app import app
+import json
+test_client = app.test_client()
+
+def test_index_response_OK():
+    assert test_client.get("/").status == "200 OK"
+
+def test_data_response_OK():
+    assert test_client.get("/data").status == "200 OK"
+
+def test_constituencies_response_OK():
+    constituencies = []
+    with open("tests\\constituencies.json") as f_in:
+        constituencies = json.loads(f_in.read())["constituencies"]
+    
+    for constituency in constituencies:
+        path = "/results/{}".format(constituency)
+        assert test_client.get(path).status == "200 OK"
+
+
